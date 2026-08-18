1c2c5e69fcce86d9d44ab36eee3b2cdce70e3ffb
Joao Barion <joaoguibarion@gmail.com>
2026-08-18 17:01:08 -0300

    test: add unit tests forremoveWord in WordListNotifier

--- test/word_list_notifier_test.dart
+++ test/word_list_notifier_test.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:growlexicon/providers/word_list_provider.dart';
@@ -7,14 +8,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main (){
   TestWidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.setMockInitialValues({});
+
+  //addWord
   test('addWord adiciona uma palavra ao estado', () async {
     //Arrange - prepara o cenário 
     final container = ProviderContainer();
     addTearDown(container.dispose);
 
     await container.read(wordListProvider.future);
-
     final notifier = container.read(wordListProvider.notifier);
+
     final tamanhoInicial = container.read(wordListProvider).value!.length;
 
     //Act - executa a ação
@@ -25,6 +28,29 @@ void main (){
     //Assert - verifica o resultado 
     final tamanhoFinal = container.read(wordListProvider).value!.length;
     expect(tamanhoFinal, tamanhoInicial+1);
+
+  });
+
+  //removeWord
+  test('removeWord retira uma palavra do estado', () async {
+    //Arrange - prepara o cenário 
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    await container.read(wordListProvider.future);
+    final notifier = container.read(wordListProvider.notifier);
+
+    notifier.addWord(
+      const Word(term: 'temp', meaning: 'temporary', example: 'A temp word.'),
+    );
+    final tamanhoInicial = container.read(wordListProvider).value!.length;
+
+    //Act - executa a ação
+    notifier.removeWord(0);
+
+    //Assert - verifica o resultado 
+    final tamanhoFinal = container.read(wordListProvider).value!.length;
+    expect(tamanhoFinal, tamanhoInicial-1);
 
   });
 }
