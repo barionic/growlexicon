456d2442143322f791489a3afe40e1bcdccee8a3
Joao Barion <joaoguibarion@gmail.com>
2026-08-19 17:00:04 -0300

    feat: add optional id field to Word model

--- lib/models/word.dart
+++ lib/models/word.dart
@@ -1,21 +1,25 @@
 class Word {
+  final int? id;
   final String term;
   final String meaning;
   final String example;
 
   const Word({
+    this.id,
     required this.term,
     required this.meaning,
     required this.example,
   });
 
   Map<String, dynamic> toJson() => {
+    if(id != null) 'id': id,
     'term': term,
     'meaning': meaning,
     'example': example,
   };
 
   factory Word.fromJson(Map<String, dynamic> json) => Word(
+    id: json['id'] as int?,
     term: json['term'] as String, 
     meaning: json['meaning'] as String, 
     example: json['example'] as String,
